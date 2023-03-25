3c2a4edf08c16025654a24a2a2ff4843d55b781b
jobor019 <joakimborg@hotmail.com>
2023-03-25 23:30:33 +0100

    Implemented transportgö Impelemented transport

--- src/transport.h
+++ src/transport.h
@@ -3,4 +3,111 @@
 #ifndef SERIALIST_LOOPER_TRANSPORT_H
 #define SERIALIST_LOOPER_TRANSPORT_H
 
+#include <chrono>
+#include <cmath>
+
+class Meter {
+public:
+    explicit Meter(int numerator = 4, int denominator = 4)
+            : numerator(numerator), denominator(denominator) {}
+
+
+    double duration() const {
+        return numerator / static_cast<double>(denominator);
+    }
+
+
+    int get_numerator() const { return numerator; }
+
+    int get_denominator() const { return denominator; }
+
+
+private:
+    int numerator;
+    int denominator;
+};
+
+
+// ==============================================================================================
+
+class TimePoint {
+public:
+    explicit TimePoint(double tick = 0.0, double tempo = 120.0, double beat = 0.0, Meter&& meter = Meter())
+            : tick(tick), tempo(tempo), beat(beat), meter(meter) {}
+
+
+    void increment(uint64_t delta_ms) {
+        double tick_increment = static_cast<double>(delta_ms) * 0.001 * tempo / 60.0;
+        tick += tick_increment;
+        beat = fmod(beat + tick_increment, meter.duration());
+
+    }
+
+
+    double get_tick() const { return tick; }
+
+    double get_tempo() const { return tempo; }
+
+    double get_beat() const { return beat; }
+
+    const Meter& get_meter() const { return meter; }
+
+
+private:
+    double tick;
+    double tempo;
+    double beat;
+    Meter meter;
+};
+
+
+// ==============================================================================================
+
+class Transport {
+public:
+    Transport() : Transport(TimePoint(), false) {}
+
+
+    explicit Transport(bool active) : Transport(TimePoint(), active) {}
+
+
+    explicit Transport(TimePoint&& initial_time, bool active)
+            : time_point(initial_time)
+              , previous_update_time(std::chrono::system_clock::now())
+              , active(active) {}
+
+
+    void start() {
+        active = true;
+        previous_update_time = std::chrono::system_clock::now();
+    }
+
+
+    void stop() {
+        active = false;
+    }
+
+
+    [[nodiscard]]
+    const TimePoint& get_current_time() {
+        if (active) {
+            auto current_time = std::chrono::system_clock::now();
+            auto time_delta = std::chrono::duration_cast<std::chrono::milliseconds>(
+                    current_time - previous_update_time).count();
+            time_point.increment(time_delta);
+        }
+
+        return time_point;
+    }
+
+
+private:
+    TimePoint time_point;
+
+    bool active;
+    std::chrono::time_point<std::chrono::system_clock> previous_update_time;
+
+
+};
+
 #endif //SERIALIST_LOOPER_TRANSPORT_H
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
+#include "../src/transport.h"
+
+#include <chrono>
+#include <thread>
+#include <iostream>
 
 TEST_CASE("dummy test2", "[dummytag1, dummytag2]") {
     REQUIRE(1 == 1);
@@ -8,4 +14,29 @@ TEST_CASE("dummy test2", "[dummytag1, dummytag2]") {
 
 TEST_CASE("dummy test2", "[dummytag3]") {
     REQUIRE(0 < 1);
+}
+
+TEST_CASE("transport test", "[transport]") {
+    Transport transport(TimePoint(0.0, 60.0, 0.0, Meter(4, 4)), true);
+
+    REQUIRE_THAT(transport.get_current_time().get_tick(), Catch::Matchers::WithinAbs(0.0, 1e-5));
+    REQUIRE_THAT(transport.get_current_time().get_tempo(), Catch::Matchers::WithinAbs(60.0, 1e-5));
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    REQUIRE_THAT(transport.get_current_time().get_tick(), Catch::Matchers::WithinAbs(1.0, 0.1));
+
+    transport = Transport(TimePoint(0.0, 180.0, 0.0, Meter(4, 4)), true);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    REQUIRE_THAT(transport.get_current_time().get_tick(), Catch::Matchers::WithinAbs(3.0, 0.1));
+
+    transport = Transport(TimePoint(0.0, 20.0, 0.0, Meter(4, 4)), true);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    REQUIRE_THAT(transport.get_current_time().get_tick(), Catch::Matchers::WithinAbs(1.0 / 3.0, 0.1));
+
+
 }
